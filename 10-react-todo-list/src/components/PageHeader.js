a9f5935fd34cac065708cdb8a6a0c3f769ba86ff
Ilia <51325702+treger78@users.noreply.github.com>
2022-06-21 17:44:09 +0300

    Add files via upload

---
+++ 10-react-todo-list/src/components/PageHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>todos</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
